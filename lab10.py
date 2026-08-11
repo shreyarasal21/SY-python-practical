d16a3410526e625a38528fdcefae9194d591d570
shreyarasal21 <rasalshreya9@gmail.com>
2026-08-11 13:15:42 +0530

    Add files via upload

---
+++ lab10.py
@@ -0,0 +1,10 @@
+name=input("Enter Customer Name:")
+feedback=input("Enter feedback:")
+
+name=(name.title())
+feedback=(feedback.strip().lower())
+
+print("*******Customer Feedback*******")
+
+print("Enter Customer Name:",name)
+print("Enter feedback:",feedback)
