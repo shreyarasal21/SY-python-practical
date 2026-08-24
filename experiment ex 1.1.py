42c397f9a490a41edd7621bdaae6ee16e9ec66e0
shreyarasal21 <rasalshreya9@gmail.com>
2026-08-24 10:37:49 +0530

    Add files via upload

---
+++ experiment ex 1.1.py
@@ -0,0 +1,30 @@
+name=input("Enter your name: ")
+
+
+
+marks1=float(input("Enter your marks of subject1: "))
+
+marks2=float(input("Enter your marks of subject2: "))
+marks3=float(input("Enter your marks of subject3: "))
+
+total = marks1+marks2+marks3
+
+average=total / 3
+
+
+
+
+
+print("======== Display score card ==========")
+
+print("subject1", marks1)
+
+print("subject2", marks2)
+
+print("subject3", marks3)
+
+print("Total marks", total)
+
+print("Average", round (average))
+
+print("---------------")
