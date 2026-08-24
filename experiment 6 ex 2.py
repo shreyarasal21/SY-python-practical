d60ef2a519fcbe13e04bee2065faeec9408315dd
shreyarasal21 <rasalshreya9@gmail.com>
2026-08-24 10:42:31 +0530

    Add files via upload

---
+++ experiment 6 ex 2.py
@@ -0,0 +1,10 @@
+rows = 7
+cols = 30
+
+for i in range(rows):
+    for j in range (cols):
+        if i == 0 or i == rows - 1 or j == 0 or j == cols - 1:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
