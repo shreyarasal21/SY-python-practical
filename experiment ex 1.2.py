5345b42a4aedc07bba08692f8d3d888b404c0275
shreyarasal21 <rasalshreya9@gmail.com>
2026-08-24 10:38:09 +0530

    Add files via upload

---
+++ experiment ex 1.2.py
@@ -0,0 +1,20 @@
+name=(input("Enter employee name:"))
+
+role=(input("Enter employee role:"))
+
+salary=int(input("Enter employee salary:"))
+
+
+print("==========")
+
+print(" EMPLOYEE IDENTITY CARD ")
+
+print("============")
+
+print("name", name)
+
+print("role", role)
+
+print("salary", format(salary))
+
+print("======================")
