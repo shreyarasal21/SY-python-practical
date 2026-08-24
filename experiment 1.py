498ef28ec978f6b9922f84d4143a142d3fb3ad6b
shreyarasal21 <rasalshreya9@gmail.com>
2026-08-24 10:37:31 +0530

    Add files via upload

---
+++ experiment 1.py
@@ -0,0 +1,13 @@
+print("========Student Information=========")
+
+name=input("Enter your name:")
+age=input("Enter your age:")
+address=input("Enter your address:")
+PRN=input("Enter your PRN:")
+
+print ("=======Show Student Information=========")
+
+print ("name is:",name)
+print ("age is :",age)
+print("address is:",address)
+print ("PRN is:",PRN)
